13c38709acea9a7636140e9b96020213f5fa4693
ToyohikoUchida <tyhkucd@gmail.com>
2024-08-25 17:10:38 +0900

    2nd commit

---
+++ ScreenTransition/ScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionApp.swift
+//  ScreenTransition
+//
+//  Created by 内田豊彦 on 2024/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
